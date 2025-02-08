659d530cc340d0723b2f3d968b55d5c476427128
DavidSebi27 <730630@student.inholland.nl>
2025-02-08 18:31:58 +0100

    Add project files.

---
+++ MvcWhatsUp/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MvcWhatsUp.Models;
+
+namespace MvcWhatsUp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult SendMessage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string SendMessage(string name, string message)
+        {
+            return $"Message {message} has been sent by {name}";
+        }
+
+        public IActionResult User()
+        {
+            return View();
+        }
+        [HttpPost]
+        public string User(string name, string password)
+        {
+            return $"Logging {name} in...";
+        }
+    }
+}
